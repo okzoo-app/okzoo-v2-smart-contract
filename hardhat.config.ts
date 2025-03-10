e112dde7f42d39fca8d6d7d66a604e27c81b62cb
Quang <quangkl198@gmail.com>
2025-03-10 18:33:00 +0700

    fix: lint

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -154,7 +154,20 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: "0.8.17",
+                version: "0.8.18",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                        details: {
+                            yul: true,
+                        },
+                    },
+                    viaIR: false,
+                },
+            },
+            {
+                version: "0.8.20",
                 settings: {
                     optimizer: {
                         enabled: true,
