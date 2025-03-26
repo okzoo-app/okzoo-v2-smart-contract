1e131a6de71f0dd771fed01eb7cce97618f25837
Quang <quangkl198@gmail.com>
2025-03-26 18:12:44 +0700

    feat: update evolve function to remove onlyUser modifier and add new error handling for evolution stages

--- scripts/config.ts
+++ scripts/config.ts
@@ -11,8 +11,8 @@ export const Config = {
 };
 
 export const ConfigOkzooV2 = {
-    owner: "", // TODO: set owner
-    verifier: "0xDb25A4DFC2fbCdb5E7E886987E443fA23247238D",
+    owner: "0xe492BE5D1f9C0DA726C560Bc451835AfC5568313", // TODO: set owner
+    verifier: "0xe492BE5D1f9C0DA726C560Bc451835AfC5568313",
     domain: "OKZOO",
     version: "1",
 };
