b169ad701ca1652ea2cd3258f765fa72d83b78c9
Quang <quangkl198@gmail.com>
2025-04-02 17:47:39 +0700

    refactor: clean up bsc-testnet.json and improve IStaking interface formatting
    - Removed unnecessary whitespace and improved formatting in bsc-testnet.json for better readability.
    - Updated IStaking interface to enhance readability by formatting function parameters and event declarations.
    - Ensured consistency in code style across the project.

--- contracts/interfaces/IStaking.sol
+++ contracts/interfaces/IStaking.sol
@@ -13,7 +13,7 @@ interface IStaking {
         uint256 aprBonus;
     }
 
-    event Staked(address indexed user,bytes32 indexed stakeRequestId, uint256 amount, uint256 lockPeriod);
+    event Staked(address indexed user, bytes32 indexed stakeRequestId, uint256 amount, uint256 lockPeriod);
 
     event Claimed(address indexed user, bytes32 indexed unstakeRequestId, uint256 amount, uint256 reward);
     event EmergencyWithdrawn(address indexed user, uint256 amount);
@@ -29,21 +29,25 @@ interface IStaking {
         bool claimed;
     }
 
-    function initialize(address _owner, address _stakeToken,address _rewardToken,uint256 _maxCap,uint256 _minStakeAmount, Tier[] calldata _tiers, LockPeriod[] calldata _lockPeriods) external;
+    function initialize(
+        address _owner,
+        address _stakeToken,
+        address _rewardToken,
+        uint256 _maxCap,
+        uint256 _minStakeAmount,
+        Tier[] calldata _tiers,
+        LockPeriod[] calldata _lockPeriods
+    ) external;
 
     function getTier(uint256 amount) external view returns (uint256);
     function getAPR(uint256 amount) external view returns (uint256);
     function getPeriodBonus(uint256 daysLocked) external view returns (uint256);
-
 
     function pause() external;
     function unpause() external;
 
     function stake(uint256 amount, uint256 lockPeriod) external;
     function claim(bytes32 stakeRequestId) external;
-
-
-
 
     // // Getters
     // function stakeToken() external view returns (IERC20Upgradeable);
