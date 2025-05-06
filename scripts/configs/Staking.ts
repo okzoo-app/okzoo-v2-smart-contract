b7007089bc1e9d246764711006b0d3d637875291
Quang <quangkl198@gmail.com>
2025-05-06 11:04:16 +0700

    chore(config): update staking parameters and APR values
    - Adjusted staking start and end times.
    - Increased max cap from 100,000 to 30,000,000.
    - Updated min stake amount from 10 to 100.
    - Modified APR values and staking tiers for better reward distribution.

--- scripts/configs/Staking.ts
+++ scripts/configs/Staking.ts
@@ -6,41 +6,41 @@ const config = {
     stakeToken: requireEnv("MAIN_TOKEN_ADDRESS"),
     rewardToken: requireEnv("MAIN_TOKEN_ADDRESS"),
 
-    startTime: dateToUnixTimestamp("2025-05-25 00:00:00 UTC").toString(),
-    endTime: dateToUnixTimestamp("2025-05-25 00:00:00 UTC").toString(),
+    startTime: dateToUnixTimestamp("2025-05-06 00:00:00 UTC").toString(),
+    endTime: dateToUnixTimestamp("2026-05-06 00:00:00 UTC").toString(),
 
-    maxCap: parseUnits("100000", 18).toString(),
-    minStakeAmount: parseUnits("10", 18).toString(),
+    maxCap: parseUnits("30000000", 18).toString(),
+    minStakeAmount: parseUnits("100", 18).toString(),
     tiers: [
         {
-            minStake: parseUnits("10", 18).toString(),
-            maxStake: parseUnits("100", 18).toString(),
-            baseAPR: parseUnits("140", 0).toString(),
-        },
-        {
             minStake: parseUnits("100", 18).toString(),
-            maxStake: parseUnits("500", 18).toString(),
-            baseAPR: parseUnits("180", 0).toString(),
-        },
-        {
-            minStake: parseUnits("500", 18).toString(),
             maxStake: parseUnits("1000", 18).toString(),
-            baseAPR: parseUnits("225", 0).toString(),
+            baseAPR: parseUnits("100", 0).toString(),
         },
         {
             minStake: parseUnits("1000", 18).toString(),
             maxStake: parseUnits("5000", 18).toString(),
-            baseAPR: parseUnits("295", 0).toString(),
+            baseAPR: parseUnits("140", 0).toString(),
         },
         {
             minStake: parseUnits("5000", 18).toString(),
             maxStake: parseUnits("10000", 18).toString(),
-            baseAPR: parseUnits("375", 0).toString(),
+            baseAPR: parseUnits("190", 0).toString(),
         },
         {
             minStake: parseUnits("10000", 18).toString(),
+            maxStake: parseUnits("50000", 18).toString(),
+            baseAPR: parseUnits("240", 0).toString(),
+        },
+        {
+            minStake: parseUnits("50000", 18).toString(),
             maxStake: parseUnits("100000", 18).toString(),
-            baseAPR: parseUnits("460", 0).toString(),
+            baseAPR: parseUnits("275", 0).toString(),
+        },
+        {
+            minStake: parseUnits("100000", 18).toString(),
+            maxStake: parseUnits("30000000", 18).toString(),
+            baseAPR: parseUnits("300", 0).toString(),
         },
     ],
     lockPeriods: [
